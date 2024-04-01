3436e4318f8b3560687e71230509c34cfee808bf
Mehedi <mehedinoorkhan16@gmail.com>
2024-04-01 23:44:50 +0600

    Mehedi's fxml files v1 have been uploaded

---
+++ src/mainpkg/DashboardFinanceOfficerController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mehedi
+ */
+public class DashboardFinanceOfficerController implements Initializable {
+
+    @FXML
+    private Label nameLabel1;
+    @FXML
+    private Label DesignationLabel1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    @FXML
+    private void takeAppointmentofChiefMedicalOfficer(ActionEvent event) {
+    }
+    @FXML
+    private void logout(ActionEvent event) {
+    }
+
+    @FXML
+    private void createersonnelsInformationButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewRequestfromFinancialDepartmentButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewWorkScheduleButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void arrangeMeetingButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void delegateTaskButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewGradeSentByFaculty(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewLeaveApplications(ActionEvent event) {
+    }
+    
+}
---
+++ src/mainpkg/FinancialPolicySceneController.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mehedi
+ */
+public class FinancialPolicySceneController implements Initializable {
+
+    @FXML
+    private TextArea writeFinancialPolicyTA;
+    @FXML
+    private TextArea showFinancialPolicyTA;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void createFinancialPolicyButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void sendToDOAButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void showFinancialPolicyButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void logoutButtonMouseOnClick(ActionEvent event) {
+    }
+    
+}
